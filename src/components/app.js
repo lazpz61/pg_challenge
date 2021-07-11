9acb4abf54cdcd81ab80558b905f162c702d8fed
LazaroMPerez <lazpz61@gmail.com>
2021-07-11 13:38:39 -0400

    attempting to render the product date

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,30 @@
+//React imports
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+// internal imports
+import DisplayProducts from "./products/display";
+
+//
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>Laz's E-Commerce Web App</h1>
+      <div>
+
+        <BrowserRouter>
+          <div className='app'>
+
+            
+            <h1>Laz's E-Commerce Web App</h1>
+            <Switch>
+              <Route path="/display" component={DisplayProducts}/>
+            </Switch>
+            
+          </div>
+
+        </BrowserRouter>
+
       </div>
     );
   }
