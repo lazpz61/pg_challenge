790e9a93eaf8a3f2dbd18d9c349cfd8ce12ce02f
LazaroMPerez <lazpz61@gmail.com>
2021-07-12 12:45:41 -0400

    rendered items on page

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,5 @@
 //React imports
 import React, { Component } from 'react';
-import axios from 'axios';
 
 
 export default class App extends Component {
@@ -12,22 +11,44 @@ export default class App extends Component {
     }
 
       this.componentDidMount = this.componentDidMount.bind(this);
+      this.renderProducts = this.renderProducts.bind(this);
   }
 
-  componentDidMount(){
+componentDidMount(){
     fetch('https://fakestoreapi.com/products')
-              .then(res=>res.json())
-              .then(res=> this.setState({
-                products: res
+              .then(response=>response.json())
+              .then(response=> this.setState({
+                products: response
               }))
               .catch(error => console.log("error bringing in products", error))
+             
+  }
+  
+  renderProducts(){
+    return this.state.products.map(item => {
+      return <div> </div>
+    })
+
   }
 
   render() {
+    const { products} = this.state;
     return (
           <div className='app'>
-
+            
             <h1>Laz's E-Commerce Web App</h1>
+          
+          <ul>
+            {products.map(product => (
+              <div className="product-display" key={product.id}>
+              
+              <img src={product.image} />
+              {product.title}
+              {product.price}
+              {product.description}
+              </div>
+            ))}
+          </ul>
            
             
           </div>
