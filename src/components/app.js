8578a538ee19a501333af7d3063fa5b7c10ccf5e
LazaroMPerez <lazpz61@gmail.com>
2021-07-12 01:50:40 -0400

    trying to render the products on page

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,6 @@
 //React imports
 import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-// internal imports
-import DisplayProducts from "./products/display";
 
 //
 
@@ -12,7 +9,7 @@ export default class App extends Component {
     super(props)
 
     this.state = {
-      productData: []
+      products: []
     }
 
       this.ComponentDidMount = this.ComponentDidMount.bind(this);
@@ -20,32 +17,21 @@ export default class App extends Component {
 
   ComponentDidMount(){
     fetch('https://fakestoreapi.com/products')
-              .then(response=>response.json())
-              .then(responseProducts => this.setState({
-                productData: [responseProducts]
+              .then(res=>res.json())
+              .then(res => this.setState({
+                products: res
               }))
               .catch(error => console.log("error bringing in products", error))
   }
 
   render() {
     return (
-      <div>
-
-        <BrowserRouter>
           <div className='app'>
 
-          
             <h1>Laz's E-Commerce Web App</h1>
-            {/* <Switch>
-              <Route path="/display" component={DisplayProducts}/>
-            </Switch> */}
-            {this.state.productData}
+           
             
           </div>
-
-        </BrowserRouter>
-
-      </div>
     );
   }
 }
